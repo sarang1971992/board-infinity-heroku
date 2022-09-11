1408662e12920ccc8c114cd18f16ea4f6bf55151
sarang <sarangbadgujar4@gmail.com>
2022-09-11 16:41:52 +0530

    Board infinity heroku first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import fs from 'fs';
+//import userapi form 'userapi.json';
+
+
+
+const app = express();
+const port = '8000';
+ 
+app.get('/student/getDetails',(req,res) =>{
+    fs.readFile('./API/userapi.json',"utf-8",(err,data) =>{
+        
+        if(err){
+            return err;
+        }
+
+        res.end(data);
+  });
+});
+
+
+ 
+
+//Server
+app.listen(port, () =>{
+    console.log("Server Running")
+})
